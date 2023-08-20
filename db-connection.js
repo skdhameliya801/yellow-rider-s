53e87cd5206ddf2cbccfbb3f3a5fc4ddb7518597
skdhameliya <smitdhameliya10@gmail.com>
2023-08-20 12:21:33 -0400

    changed srvr

--- db-connection.js
+++ db-connection.js
@@ -1,6 +1,9 @@
 // smitdhameliya801
 const mongoose = require("mongoose")
-mongoose.connect("mongodb+srv://smit801:smit801@cluster0.hotxn1c.mongodb.net/yellow-ride-2", {
+
+// mongodb+srv://smit801:smit801@cluster0.hotxn1c.mongodb.net/yellow-ride-2
+
+mongoose.connect("mongodb+srv://smitdhameliya801:smitdhameliya801@cluster1.q6tlat2.mongodb.net/yellow-ride-2", {
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(() => console.log("connection success...")).catch((error) => console.log(error))
